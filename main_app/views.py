7a08bd13d773589784962187b8734ea9f6622b7c
korycfitz <korycfitz@gmail.com>
2023-10-31 00:26:38 -0400

    create a loan index page in main_app/templates/loans and code a basic home view

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Add the Cat class & list and view function below the imports
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Welcome to LendAFriend</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Add new view
+def loan_index(request):
+  return render(request, 'loans/index.html', { 'loans': loans })
+
 class Loan:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, dateCreated, debtor, creditor, amount, dateDue, description):
+  def __init__(self, dateCreated, creditor, debtor, amount, dateDue, description):
     self.dateCreated = dateCreated
-    self.debtor = debtor
     self.creditor = creditor
+    self.debtor = debtor
     self.amount = amount
     self.dateDue = dateDue
     self.description = description
@@ -15,11 +27,3 @@ loans = [
   Loan('11/1/23', 'Mike', 'Kory', 500, '12/31/23', "books"),
   Loan('11/16/23', 'John', 'Smith', 85, '1/1/24', "ticket"),
 ]
-
-# Define the home view
-def home(request):
-  return HttpResponse('<h1>Welcome to LendAFriend</h1>')
-
-def about(request):
-  return render(request, 'about.html')
-
