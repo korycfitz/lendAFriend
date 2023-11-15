7a55eed0ab43abfd103d02e8fa9f2eef0aee6ad2
korycfitz <korycfitz@gmail.com>
2023-11-15 13:07:44 -0500

    add model for Loan

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Loan(models.Model):
+  dateCreated = models.DateField()
+  creditor = models.CharField() #this will be linked to the current object id and will need to  eventuall add logic to make debtor to the current user_id
+  debtor = models.CharField() #will need to change once we input the either a model for the other user id or a way to search for them and set equal to this
+  amount = models.IntegerField()
+  dateDue = models.DateField()
+  description = models.TextField(max_length=250)
+
