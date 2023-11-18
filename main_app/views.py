1a065e933d0adc55b15cb4797ba0648f2dd79ec1
korycfitz <korycfitz@gmail.com>
2023-11-17 23:02:50 -0500

    add the path for details view to main_app/urls.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # Add the Loan class & list and view function below the imports
 from .models import Loan
+
 # Define the home view
 def home(request):
   return render(request, 'home.html')
