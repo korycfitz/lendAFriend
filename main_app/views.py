47b9e2145a216d3ebc7dccd5c4cbb2fa8313f57c
korycfitz <korycfitz@gmail.com>
2023-11-17 23:06:55 -0500

    define view function for cat_detail

--- main_app/views.py
+++ main_app/views.py
@@ -14,6 +14,10 @@ def loan_index(request):
   loans = Loan.objects.all()
   return render(request, 'loans/index.html', { 'loans': loans })
 
+def loan_detail(request, loan_id):
+  loan = Loan.objects.get(id=loan_id)
+  return render(request, 'loans/detail.html', { 'loan': loan })
+
 class Loan:  # Note that parens are optional if not inheriting from another class
   def __init__(self, dateCreated, creditor, debtor, amount, dateDue, description):
     self.dateCreated = dateCreated
