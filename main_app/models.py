d18f2e3121255f57be744267948c2ac869cc8166
korycfitz <korycfitz@gmail.com>
2023-11-21 12:57:54 -0500

    fix error now allowing me to save to db

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Loan(models.Model):
   dateCreated = models.DateField()
-  creditor = models.CharField() #this will be linked to the current object id and will need to  eventuall add logic to make debtor to the current user_id
-  debtor = models.CharField() #will need to change once we input the either a model for the other user id or a way to search for them and set equal to this
+  creditor = models.CharField()
+  debtor = models.CharField()
   amount = models.IntegerField()
   dateDue = models.DateField()
   description = models.TextField(max_length=250)
