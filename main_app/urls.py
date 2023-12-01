42ad505cefd834408b7b7f088e09e3dae293389b
korycfitz <korycfitz@gmail.com>
2023-11-30 23:51:44 -0500

    add new routes in urls.py to add edit and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('loans/', views.loan_index, name='loan-index'),
   path('loans/<int:loan_id>/', views.loan_detail, name='loan-detail'),
   path('loans/create/', views.LoanCreate.as_view(), name='loan-create'),
+  path('loans/<int:pk>/update/', views.LoanUpdate.as_view(), name='loan-update'),
+  path('laons/<int:pk>/delete/', views.LoanDelete.as_view(), name='loan-delete'),
 ]
