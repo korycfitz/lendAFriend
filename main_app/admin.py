800ae697cba356bc3002e9f5317067cd9b090382
korycfitz <korycfitz@gmail.com>
2023-12-11 19:21:39 -0500

    register new debt model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Loan
+from .models import Loan, Debt
 
 # Register your models here
 admin.site.register(Loan)
+# register the new Debt Model 
+admin.site.register(Debt)
